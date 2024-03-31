493b41f90713f2706770afd9cd6589ca1c54ad8c
Abir <rifatabir21@gmail.com>
2024-04-01 02:39:00 +0600

    zoom function set directly by number

--- src/components/zoom-canvas/index.js
+++ src/components/zoom-canvas/index.js
@@ -1,4 +1,4 @@
-import SelectBox from "components/select-box";
+import InputField from "components/input-field";
 import AppIcon from "utils/app-icon";
 import useZoomCanvas from "./useZoomCanvas";
 const ZoomCanvas = () => {
@@ -12,27 +12,21 @@ const ZoomCanvas = () => {
         >
           <AppIcon iconName="icon-minus" classes="w-3 h-3" />
         </div>
-        <SelectBox
-          position="top"
-          value={`${zoom.value}%`}
-          innerContainerClass="h-3 w-9"
+
+        <InputField
+          // innerContainerClass="h-3 w-9"
+          placeholder=""
+          type="number"
+          value={zoom.value}
+          containerClass="w-per-45"
           onChange={(value) => zoom.setZoom(value)}
-          options={[
-            {title: "10%", value: 10},
-            {title: "25%", value: 25},
-            {title: "35%", value: 35},
-            {title: "50%", value: 50},
-            {title: "100%", value: 100},
-            {title: "150%", value: 150},
-            {title: "200%", value: 200}
-          ]}
         />
 
-        {/* <span
+        <span
           style={{position: "relative", top: -1, left: -65, fontSize: "14px"}}
         >
           %
-        </span> */}
+        </span>
         <div
           onClick={() => zoom.setZoom(zoom.value + 10)}
           className="action-button"
